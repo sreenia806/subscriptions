2bda3bac29d3835333940b65ef73eae235802955
Sreenivasulu Avula <sreeniavasulu@gmail.com>
2020-07-28 15:13:50 +0530

    plan unit tests added

--- tests/Unit/UnitTest.php
+++ tests/Unit/UnitTest.php
@@ -8,42 +8,30 @@ use Tests\TestCase;
 class PostTest extends TestCase
 {
 
-    public function test_can_create_post() {
+    public function test_can_show_plan() {
 
-        $data = [
-            'title' => $this->faker->sentence,
-            'content' => $this->faker->paragraph,
-        ];
+        $this->get(route('plans.show', 'fr'))
+            ->assertStatus(200);
+    }
+
+
+    public function test_can_list_posts() {
+
+        $this->get(route('plans'))
+            ->assertStatus(200)
+            ->assertJson($posts->toArray())
+            ->assertJsonStructure([
+                '*' => [ 'code', 'name', 'monthly_cost', 'yearly_cost' ],
+            ]);
+    }
+
+    /*
+    public function test_can_preview_subscriptions() {
 
         $this->post(route('posts.store'), $data)
             ->assertStatus(201)
             ->assertJson($data);
     }
+    */
 
-    public function test_can_show_plan() {
-
-        $this->get(route('posts.show', $post->id))
-            ->assertStatus(200);
-    }
-
-    public function test_can_delete_post() {
-
-        $post = factory(Post::class)->create();
-
-        $this->delete(route('posts.delete', $post->id))
-            ->assertStatus(204);
-    }
-
-    public function test_can_list_posts() {
-        $posts = factory(Post::class, 2)->create()->map(function ($post) {
-            return $post->only(['id', 'title', 'content']);
-        });
-
-        $this->get(route('posts'))
-            ->assertStatus(200)
-            ->assertJson($posts->toArray())
-            ->assertJsonStructure([
-                '*' => [ 'id', 'title', 'content' ],
-            ]);
-    }
 }
